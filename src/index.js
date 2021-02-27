44e3c1ce7cc1f16df6bec585ccaf7acc89cff8b2
KokutoSan <KokutoSan@users.noreply.github.com>
2021-02-27 12:10:09 +0100

    When someone wins, highlight the three squares that caused the win

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
+  const className = props.winner ? 'square square-winner' : 'square';
   return (
     <button
-      className="square"
+      className={className}
       onClick={props.onClick}
     >
       {props.value}
@@ -30,9 +31,18 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
+    let isWinner = false;
+
+    for (let index = 0; index < this.props.winnerLine.length && isWinner === false; index++) {
+      if (this.props.winnerLine[index] === i) {
+        isWinner = true;
+      }
+    }
+
     return (
       <Square
         key={i}
+        winner={isWinner}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -132,9 +142,11 @@ class Game extends React.Component {
     }
 
     let status;
+    let winnerLine = [];
 
     if (winner) {
-      status = 'Winner: ' + winner;
+      status = 'Winner: ' + winner.square;
+      winnerLine = winner.line;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
@@ -144,6 +156,7 @@ class Game extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
+            winnerLine={winnerLine}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
@@ -171,7 +184,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {square: squares[a], line: lines[i]};
     }
   }
   return null;
